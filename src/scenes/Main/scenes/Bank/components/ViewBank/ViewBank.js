e149023a45c0419ad65ff385eac298d662ef7376
Anthony Herco <aherco@lion.lmu.edu>
2017-10-14 17:49:23 -0700

    Removed grid_props

--- src/scenes/Main/scenes/Bank/components/ViewBank/ViewBank.js
+++ src/scenes/Main/scenes/Bank/components/ViewBank/ViewBank.js
@@ -3,14 +3,8 @@ import StackGrid from 'react-stack-grid';
 
 export default class ViewBank extends Component {
   render = () => {
-    const grid_props = {
-      monitorImagesLoaded: true,
-      columnWidth: '20%',
-      gutterWidth: '1em',
-      gutterHeight: '1em',
-    }
     const card_list = this.props.card_list;
-    const grid = <StackGrid {...this.grid_props}>{card_list}</StackGrid>;
+    const grid = <StackGrid columnWidth='20%'>{card_list}</StackGrid>;
     return <div className='ViewBank'>{grid}</div>;
   }
 }
