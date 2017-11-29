ba974eb7b8f56cae2e9ea209769ccbac0384be46
Unknown <alari@lion.lmu.edu>
2017-11-28 22:52:26 -0800

    Set Up of preliminary test file
    Need to read up on jest more, but this should be the first simple unit test

---
+++ unit-tests/integration-tests.js
@@ -0,0 +1,12 @@
+/* test('on click makes call to localhost', () => {
+  //https://i.imgur.com/KOoBDaK.gif is the image link we will use
+  // This test is supposed to:
+  // Open MemeBank's webpage
+  // input that image link into FieldInputBank
+  // click on the button
+  //check that it returned the link
+  // another test for hitting enter, everything will be the same except this line
+  // once cards is working again, test that it returned that too
+  expect();
+});
+*/
