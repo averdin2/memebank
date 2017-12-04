eda99eb31707b6886fe3f439712921d1a342eb3b
Anthony Herco <aherco@lion.lmu.edu>
2017-12-03 22:04:42 -0800

    In-house add bank button

--- src/scenes/Main/components/BankList/components/AddBankButton/AddBankButton.js
+++ src/scenes/Main/components/BankList/components/AddBankButton/AddBankButton.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
-// Nested Components
-import { PlusButton } from 'react-svg-buttons';
+// Style
+import './AddBankButton.css';
 
 export default class AddBankButton extends Component {
 
   render () {
-    return <PlusButton className='AddBankButton' size={70} color='#4679BD' {...this.props}/>;
+    const text = <h4>add</h4>;
+    return <div className='AddBankButton' {...this.props}>{text}</div>;
   }
 }
